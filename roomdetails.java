03d62de6b72b6977605da1c26a329e1bd9f542ee
VinitRN <01fe22bca044@kletech.ac.in>
2024-02-10 11:53:48 +0530

    program for room details

---
+++ roomdetails.java
@@ -0,0 +1,21 @@
+public class roomdetails {
+    int room_no;
+    public void print()
+    {
+       if(room_no==101)
+       {
+        System.out.println("Your room is in first floor");
+       }
+
+       else if(room_no==102)
+       {
+        System.out.println("Your room is in second floor");
+       }
+
+       else
+       {
+        System.out.println("Your room is in Third floor");
+       }
+    }
+}
+
